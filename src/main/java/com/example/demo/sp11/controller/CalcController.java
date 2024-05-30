a1487464fb1342bd4e43a281df449941374a09d7
student <student@WIN-H7M01J10L01>
2024-05-30 16:04:11 +0900

    SP11 Step1

--- src/main/java/com/example/demo/sp11/controller/CalcController.java
+++ src/main/java/com/example/demo/sp11/controller/CalcController.java
@@ -1,7 +1,5 @@
 package com.example.demo.sp11.controller;
 
-import java.util.Random;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,27 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/sp10")
+@RequestMapping("/sp11")
 public class CalcController {
 
 	@GetMapping("/calc")
 	public String index() {
-		return "sp10/omikuji";
+		return "sp11/calc";
 	}
 
 	@GetMapping("/add")
 	public String index(
 			@RequestParam("num1") Integer num1,
+			@RequestParam("num2") Integer num2,
 			Model model
 	) {
-		String[] kuji = {"大吉", "吉", "凶", "吉", "小吉", "吉"};
-
-		Random rand = new Random();
-		int num = rand.nextInt(6);
-
-		String result = kuji[num];
+		
+		Integer result = num1 + num2;
 		model.addAttribute("result", result);
 
-		return "sp10/omikuji";
+		return "sp11/calc";
 	}
 }
